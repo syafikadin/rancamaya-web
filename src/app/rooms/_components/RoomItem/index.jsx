7333ef55e3819271b9be01c7c2937cdb1fb4a7a2
syafikadin <syafik.adin222@gmail.com>
2025-03-01 00:10:32 +0700

    Update Card on Rooms

--- src/app/rooms/_components/RoomItem/index.jsx
+++ src/app/rooms/_components/RoomItem/index.jsx
@@ -4,23 +4,26 @@ import Link from "next/link";
 
 // const SUPABASE_ROOMS_URL = process.env.NEXT_PUBLIC_SUPABASE_IMGS_URL;
 
-function RoomItem({ id, imgPath, price, title }) {
+function RoomItem({ id, imgPath, price, title, location, rating }) {
   return (
-    <div className={styles.roomsGrid}>
-      <div className={styles.roomItem}>
+    <div className={styles.roomItem}>
+      <Link href={`rooms/${id}`}>
         <div className={styles.imgOverlay}>
-          <Image fill src={`/${imgPath}`} alt="" />
-          {/* <Image fill src="/room2.jpg" alt="" /> */}
+          <Image fill src={`/${imgPath}`} alt={title} className={styles.image} />
         </div>
         <div className={styles.roomDescription}>
-          <div>
+          <div className={styles.header}>
             <h2 className={styles.roomTitle}>{title}</h2>
-            <Link href={`rooms/${id}`}>
-              From Rp{price.toLocaleString("id-ID")} / Night
-            </Link>
+            <span className={styles.rating}>8.7</span>
           </div>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad asperiores
+            explicabo blanditiis voluptates at accusantium culpa possimus, incidunt
+            ducimus optio?
+          </p>
+          <p className={styles.price}>IDR {price.toLocaleString("id-ID")}</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
